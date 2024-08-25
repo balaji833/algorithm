f40ceb9fb2d6775a8389202b05621c2d7310b91f
balaji833 <balajiee4@gmail.com>
2024-08-25 19:30:23 +0530

    add the file

---
+++ arrayofstring.js
@@ -0,0 +1,7 @@
+let b = ["jeeva", "balaji", "gokul", "vasanth", "anbu", "pushparaj"]
+console.log(b[b.length - 1])
+
+let sum = 0;
+for (let i = 0; i < b.length; i++)
+    sum += b[i].length;
+console.log(sum);
