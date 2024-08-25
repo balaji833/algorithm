977a66020d96c42212691b7ab5047dd3e236ac49
balaji833 <balajiee4@gmail.com>
2024-08-25 19:10:16 +0530

    add the file

---
+++ area.js
@@ -0,0 +1,27 @@
+
+function rectangle(lenght, width) {
+    return lenght * width;
+}
+
+function square(sidelenght) {
+    return sidelenght * sidelenght;
+}
+
+function triangle(base, height) {
+    return 0.5 * base * height;
+}
+
+function circle(radius) {
+    return Math.PI * radius * radius;
+}
+
+function trapezoid(a, b, h) {
+    return 0.5 * (a + b) * h;
+}
+
+function ellipse(a, b) {
+    return Math.PI * a * b;
+}
+
+
+
