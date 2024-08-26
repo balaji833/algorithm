dca8a1ffbaabd52f66087a7e4f3ac69dd86139a9
balaji833 <balajiee4@gmail.com>
2024-08-26 21:17:15 +0530

    Delete table.js

--- table.js
+++
@@ -1,9 +0,0 @@
-table = "";
-for (i = 1; i <= 10; i++) {
-    for (j = 1; j <= 10; j++) {
-        table += (i * j).toString().padStart(3, " ") + " "
-    }
-    table += "\n"
-
-}
-console.log(table);
