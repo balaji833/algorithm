092025fcb5961427e25bca094a7040151f3c05bf
balaji833 <balajiee4@gmail.com>
2024-08-26 19:20:42 +0530

    add the file

--- arrayofstring.js
+++ arrayofstring.js
@@ -1,5 +1,11 @@
-// let b = ["jeeva", "balaji", "gokul", "vasanth", "anbu", "pushparaj"]
-// let sum = 0;
-// for (let i = 0; i < b.length; i++)
-//     sum += b[i].length;
-// console.log(sum);
+function string(array){
+let sum = 0;
+for (let i = 0; i < array.length; i++){
+    sum += array[i].length;
+  
+}
+return sum;
+
+}
+var a=string( ["balaji","vasanth","pushparaj","anbu","arvind","jeeva","vijayraj","gokul","ragunath"]);
+console.log(a)
