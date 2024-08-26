b3104b3ec4cb88694a5905d0415cd974c322e6d6
balaji833 <balajiee4@gmail.com>
2024-08-26 11:55:16 +0530

    add the file

--- consonants.js
+++ consonants.js
@@ -1,17 +1,19 @@
-var a = ["ragav", "chandru", "vasanth", "balaji", "jeeva"]
-var vowels = ["a", "e", "i", "o", "u"]
-for (i = 0; i < a.length; i++) {
-   var b = a[i];
-   var numberOfConsonants = 0;
-   for (j = 0; j < b.length; j++) {
-      if (vowels.includes(b[j])) {
-
-      }
-      else{
-         console.log(b[j]+" is a consonants")
-         numberOfConsonants++;
-      }
-
-   }
-   console.log(numberOfConsonants);
+function consonants(array) {
+    var vowels = ["a", "e", "i", "o", "u"];
+    var result = [];
+    for (var i = 0; i < array.length; i++) {
+        var b = array[i];
+        var numberOfConsonants = 0;
+        for (var j = 0; j < b.length; j++) {
+            if (!vowels.includes(b[j].toLowerCase())) {
+                numberOfConsonants++;
+            }
+        }
+        result.push(numberOfConsonants);
+    }
+    return result;
 }
+
+var name = consonants(["balaji","santhosh"]);
+console.log(name); 
+
