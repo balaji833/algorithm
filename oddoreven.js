e58f55fd7bd943c7f3ec8f7333634c1e4eff34e8
balaji833 <balajiee4@gmail.com>
2024-08-26 16:10:38 +0530

    add the file

---
+++ oddoreven.js
@@ -0,0 +1,10 @@
+function oddOrEven(number) {
+    if (number % 2 === 0) {
+        return  number + " is a Even"
+    }
+    else {
+        return number+ " is a odd"
+    }
+}
+var a = oddOrEven(2)
+console.log(a)
