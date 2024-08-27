3b024ba492522830aae909547d72787d99f3afc3
balaji833 <balajiee4@gmail.com>
2024-08-27 13:34:31 +0530

    add the file

--- applyfunction.js
+++ applyfunction.js
@@ -1,6 +1,19 @@
 function multi(a, b) {
     return a * b;
 }
-const numbers = [2, 2];
+const numbers = [2,4];
 const result = multi.apply(null, numbers);
 console.log(result)
+
+
+
+
+
+function division(a){
+    return a/2;
+}
+const number=[4];
+const ans=division.call(null,number);
+console.log(ans);
+
+
--- arrayofstring.js
+++ arrayofstring.js
@@ -1,11 +1,11 @@
-function string(array){
-let sum = 0;
-for (let i = 0; i < array.length; i++){
-    sum += array[i].length;
-  
-}
-return sum;
+function string(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i].length;
+
+    }
+    return sum;
 
 }
-var a=string( ["balaji","vasanth","pushparaj","anbu","arvind","jeeva","vijayraj","gokul","ragunath"]);
+var a = string(["balaji", "vasanth", "pushparaj", "anbu", "arvind", "jeeva", "vijayraj", "gokul", "ragunath"]);
 console.log(a);
