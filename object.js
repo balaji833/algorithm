db7ba877ab1b867c267d67d7b34d6ba04fd45cde
balaji833 <balajiee4@gmail.com>
2024-08-27 11:09:49 +0530

    add the file

---
+++ object.js
@@ -0,0 +1,8 @@
+function Number(firstnumber, secondnumber) {
+  this.firstnumber=firstnumber;
+  this.secondnumber= secondnumber;
+   this. a= this.firstnumber*this.secondnumber
+   
+}
+const mynumbers = new Number(2,2);
+console.log(mynumbers.a);
