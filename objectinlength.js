3185e4ea7997eb375cd7f1979b2ddec3b00aae7d
balaji833 <balajiee4@gmail.com>
2024-08-27 16:56:10 +0530

    add the file

--- objectinlength.js
+++ objectinlength.js
@@ -1,9 +1,11 @@
 function Length(string) {
     this.string = this.string;
     this.result = [];
+   
     for (i = 0; i < string.length; i++){
-        this.b.push([string[i].length])
+        this.result.push([string[i].length])
     }
+   
 }
 const obj = new Length(["balaji","ant"]);
 console.log(obj.result);
