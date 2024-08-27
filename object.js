ea3f1068683149add3d68b41a479d4ac7305cd23
balaji833 <balajiee4@gmail.com>
2024-08-27 12:53:25 +0530

    add the file

--- object.js
+++ object.js
@@ -8,15 +8,12 @@ function Number(firstnumber, secondnumber) {
 const mynumbers = new Number(2, 2);
 console.log(mynumbers.a);
 
-
-
-
 function Name(firstname, lastname, age, color) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.age = age;
     this.color = color;
-    this.details = this.firstname +" "+  this.lastname+" " + this.age+" " + this.color;
+    this.details = this.firstname + " " + this.lastname + " " + this.age + " " + this.color;
 }
 const mydetails = new Name("balaji", "bala", 22, "black");
 console.log(mydetails.details);
