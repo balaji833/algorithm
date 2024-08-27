74931c1f3ad7ebee863ea0684c8c7cf2c251c143
balaji833 <balajiee4@gmail.com>
2024-08-27 19:55:31 +0530

    add the file

---
+++ countVowelsInStrings.js
@@ -0,0 +1,18 @@
+const vowels = ["a", "e", "i", "o", "u"];
+function countVowelsInStrings(names) {
+    let results = [];
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        let numberOfVowels = 0;
+
+        for (let j = 0; j < name.length; j++) {
+            if (vowels.includes(name[j])) {
+                numberOfVowels++;
+            }
+        }
+        results.push(numberOfVowels);
+    }
+    return results;
+}
+let countOfVowels = countVowelsInStrings(["balaji", "santhosh","deepak","ant"]);
+console.log(countOfVowels);
