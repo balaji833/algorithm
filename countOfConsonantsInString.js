94b6e7cd4fe0cfac50fe07ad5dab97d3585bb622
balaji833 <balajiee4@gmail.com>
2024-08-27 20:05:15 +0530

    add the file

---
+++ countOfConsonantsInString.js
@@ -0,0 +1,19 @@
+function countOfConsonantsInString(names) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let result = [];
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        let numberOfConsonants = 0;
+        for (let j = 0; j < name.length; j++) {
+            if (!vowels.includes(name[j].toLowerCase())) {
+                numberOfConsonants++;
+            }
+        }
+        result.push(numberOfConsonants);
+    }
+    return result;
+}
+
+let input = countOfConsonantsInString(["balaji","santhosh"]);
+console.log(input); 
+
