18245a3173b4ebe8e6a34727c37f76d7c530809f
balaji833 <balajiee4@gmail.com>
2024-08-28 19:32:36 +0530

    modify the add

--- palindrome.js
+++ palindrome.js
@@ -2,3 +2,6 @@ function isPalindrome(str) {
         const reversedStr = str.split('').reverse().join('');
         return str === reversedStr;
     }
+    let testString="madam"
+    let result=isPalindrome(testString)
+    console.log(result);
