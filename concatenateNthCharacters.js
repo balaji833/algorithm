f7a9e7192aa6b2ffc2c46fb94462c4510e9b50a7
balaji833 <balajiee4@gmail.com>
2024-08-28 10:17:35 +0530

    add the file

---
+++ concatenateNthCharacters.js
@@ -0,0 +1,18 @@
+function concatenateNthCharacters(strings) {
+  let result = "";
+
+  for (let i = 0; i < strings.length; i++) {
+    let currentString = strings[i];
+
+    if (i < currentString.length) {
+      result += currentString[i];
+    } else {
+      result += "-";
+    }
+  }
+
+  return result;
+}
+
+let input = concatenateNthCharacters(["balaji", "santhosh", "deepak","ant"]);
+console.log(input);
