41bf8d4dd55c5a789ddaff564a0f9b4737b30564
balaji833 <balajiee4@gmail.com>
2024-08-29 10:13:42 +0530

    modify the add

---
+++ sumAdjancentPairs.js
@@ -0,0 +1,15 @@
+function sumAdjacentPairs(twonumbers) {
+    let result = [];
+    for (i = 0; i < twonumbers.length; i += 2) {
+        if (i + 1 < twonumbers.length) {
+            let sum = twonumbers[i] + twonumbers[i + 1];
+            result.push(sum);
+        }
+        else {
+            result.push(twonumbers[i])
+        }
+    }
+    return result;
+}
+let input = sumAdjacentPairs([1, 2, 3, 4, 5, 6]);
+console.log(input);
