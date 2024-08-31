41b54c524115368d89a297aef3926fe59639efc0
balaji833 <balajiee4@gmail.com>
2024-08-31 21:47:39 +0530

    modify the add

--- subractTheElement.js
+++ subractTheElement.js
@@ -2,11 +2,11 @@ function subractTheElement(numbers) {
     let result = [];
     let i = 0;
     while (i < numbers.length / 2) {
-        let sum =numbers[i]-numbers[numbers.length-1-i];
+        let sum = numbers[i] - numbers[numbers.length - 1 - i];
         result.push(sum);
         i++;
     }
     return result;
 }
-let input = subractTheElement([1,2,3,4,5]);
+let input = subractTheElement([1, 2, 3, 4, 5]);
 console.log(input);
