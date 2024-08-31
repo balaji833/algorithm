0d36410138003cb8b31292cece99c53cec7e70ba
balaji833 <balajiee4@gmail.com>
2024-08-31 21:43:50 +0530

    modify the add

---
+++ subractTheElement.js
@@ -0,0 +1,12 @@
+function subractTheElement(numbers) {
+    let result = [];
+    let i = 0;
+    while (i < numbers.length / 2) {
+        let sum =numbers[i]-numbers[numbers.length-1-i];
+        result.push(sum);
+        i++;
+    }
+    return result;
+}
+let input = subractTheElement([1,2,3,4,5]);
+console.log(input);
